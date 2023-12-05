6812a0d762a8ab0ff0e0f6339ea1daab5cd5d772
Ali Taghiev <tagiyev_eli@bk.ru>
2023-12-05 23:03:30 +0400

    Implement Dashboard window functionality
    Implemented the DashBoard window with a collapsible sidebar using WPF.
    
    - Added event handlers for the "Home" button to expand/collapse the sidebar.
    - Used animations to smoothly transition the sidebar width changes.
    - Implemented a method to close the Dashboard window and show the main application window on "Exit" button click.

--- FASTFOOD/User/DashBoard.xaml.cs
+++ FASTFOOD/User/DashBoard.xaml.cs
@@ -9,21 +9,62 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace FASTFOOD.User
 {
-    /// <summary>
-    /// Interaction logic for DashBoard.xaml
-    /// </summary>
+    
+
     public partial class DashBoard : Window
     {
+        private bool Expanded = false;
+       
+
         public DashBoard()
         {
             InitializeComponent();
         }
-        
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            GridLength OpenSize = new GridLength(180, GridUnitType.Pixel);
+            sidebar.Width = OpenSize;
+            int from, to;
+            if (Expanded)
+            {
+                from = 180;
+                to = 60;
+                Expanded = false;
+            }
+            else
+            {
+                from = 60;
+                to = 180;
+                Expanded = true;
+            }
+            Storyboard storyboard = new Storyboard();
+
+            Duration duration = new Duration(TimeSpan.FromMilliseconds(200));
+            CubicEase ease = new CubicEase { EasingMode = EasingMode.EaseOut };
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.EasingFunction = ease;
+            animation.Duration = duration;
+            storyboard.Children.Add(animation);
+            animation.From = from;
+            animation.To = to;
+            Storyboard.SetTarget(animation, sidebar);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(ColumnDefinition.MaxWidth)"));
+
+            storyboard.Begin();
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            App.Current.MainWindow.Show();
+            this.Close();
+        }
+
 
     }
 }
