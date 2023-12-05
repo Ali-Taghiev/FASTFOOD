afa6544abc0fd83185c534565fe8343b628ce7d0
Ali Taghiev <tagiyev_eli@bk.ru>
2023-12-05 19:55:20 +0400

    Add Connection_Settings Window
    Introduce a new Window, Connection_Settings, to manage MySQL server connection settings in the FASTFOOD application. This window includes input fields for MySQL server address, port, username, password, and database name, providing users with the ability to configure connection settings.

---
+++ FASTFOOD/User/Connection_Settings.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FASTFOOD.User
+{
+   
+    public partial class Connection_Settings : Window
+    {
+        public Connection_Settings()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
