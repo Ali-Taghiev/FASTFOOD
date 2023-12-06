a3c37b033280f5e171e197b8e9c61856576a5571
Ali Taghiev <tagiyev_eli@bk.ru>
2023-12-06 23:00:11 +0400

    Update User Management in settings window
    - Modify the userCreate_Click method to use SHA-256 hash for password.
    - Use SqlConnectionConfiguration for SQL Server connection details.
    - Improve exception handling and error messages.
    - Update SQL queries to use parameterized queries for better security.
    
    Related files:
    - FASTFOOD.User.settingsAddUser.xaml.cs
    - FASTFOOD.User.settings.xaml

--- FASTFOOD/User/settings.xaml.cs
+++ FASTFOOD/User/settings.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
@@ -32,6 +34,55 @@ namespace FASTFOOD.User
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+        }
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (users.IsSelected)
+            {
+                SqlConnection con = null;
+                try
+                {
+                    
+                    String query = "SELECT username as 'Username', name as 'Name', email as 'e-mail' FROM Users";
+
+                    // Get SQL Server connection details
+                    SqlConnectionConfiguration config = new SqlConnectionConfiguration();
+                    string connectionString = config.GetConnectionString();
+
+                    // Create and open a connection to the SQL Server
+                    con = new SqlConnection(connectionString);
+                    con.Open();
+
+                    // Use SqlDataAdapter to fill a DataTable with the query results
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // Populate the DataGrid with the DataTable
+                    DataGridUsers.ItemsSource = table.DefaultView;
+                    DataGridUsers.AutoGenerateColumns = true;
+                    DataGridUsers.CanUserAddRows = false;
+                }
+                catch (Exception exc)
+                {
+                    // Handle any exceptions and print the error message
+                    Console.WriteLine(exc.Message.ToString());
+                }
+                finally
+                {
+                    // Close the connection in the finally block to ensure it is closed regardless of exceptions
+                    if (con != null && con.State == System.Data.ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+        }
+
+        private void userCreate_Click(object sender, RoutedEventArgs e)
+        {
+            new settingsAddUser().ShowDialog();
+            
         }
     }
 }
---
+++ FASTFOOD/User/settingsAddUser.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace FASTFOOD.User
+{
+    /// <summary>
+    /// Interaction logic for settingsAddUser.xaml
+    /// </summary>
+    public partial class settingsAddUser : Window
+    {
+        public settingsAddUser()
+        {
+            InitializeComponent();
+        }
+
+        private void userCreate_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (userName.Text != "" && userUsername.Text != "" && userMail.Text != "" && userPassword.Password != "")
+            {
+                SqlConnectionConfiguration config = new SqlConnectionConfiguration();
+                string connectionString = config.GetConnectionString();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        con.Open();
+
+                        string hashedPassword = GetSHA256Hash(userPassword.Password);
+
+                        string query = "INSERT INTO Users (name, username, password, email) VALUES (@name, @username, @password, @email)";
+                        using (SqlCommand cmd = new SqlCommand(query, con))
+                        {
+                            cmd.Parameters.AddWithValue("@name", userName.Text);
+                            cmd.Parameters.AddWithValue("@username", userUsername.Text);
+                            cmd.Parameters.AddWithValue("@password", hashedPassword);
+                            cmd.Parameters.AddWithValue("@email", userMail.Text);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("User added successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                                this.Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error adding user.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all the fields.", "Incomplete Data", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private string GetSHA256Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in hashedBytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        
+    }
+}
