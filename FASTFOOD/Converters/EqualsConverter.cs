bc02c36979289e34f477d9fd9ebd7ddf2961be0c
Ali Taghiev <tagiyev_eli@bk.ru>
2023-12-09 00:01:21 +0400

    Changes Made Created a new service entry in the 'Services' table with the current timestamp. Utilized SCOPE_IDENTITY() to retrieve the generated service ID. Updated the 'tables' table with the actual service ID for the selected table. Checked for null values and displayed appropriate error messages. Utilized ExecuteScalar to handle the result of the query execution. Implemented try-catch blocks to handle exceptions and display meaningful error messages. Ensured proper closing of the SQL connection in the finally block. Implemented reloading of the view (LoadData()) upon successful table assignment.

---
+++ FASTFOOD/Converters/EqualsConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FASTFOOD.Converters
+{
+    public class EqualsConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            if (values[0] != null && values[1] != null)
+            {
+                if (Int32.Parse(values[0].ToString()) == Int32.Parse(values[1].ToString()))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FASTFOOD/User/Pages/Tables.xaml.cs
+++ FASTFOOD/User/Pages/Tables.xaml.cs
@@ -3,6 +3,8 @@ using FASTFOOD.User.Components;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +13,7 @@ using System.Windows.Media;
 
 namespace FASTFOOD.User.Pages
 {
-    public partial class Tables : UserControl
+    public partial class Tables : UserControl, INotifyPropertyChanged
     {
         public List<Table> NumberButtonItems { get; set; }
 
@@ -22,15 +24,15 @@ namespace FASTFOOD.User.Pages
             DataContext = this; // Set the DataContext to the current instance of the Tables class
 
         }
-        
+
 
         private void LoadData()
         {
             SqlConnectionConfiguration config = new SqlConnectionConfiguration();
             NumberButtonItems = config.GetTables();
-            
-                // Add this line to check the Id of the first item after loading data
-                System.Diagnostics.Debug.WriteLine("First item Id: " + NumberButtonItems.FirstOrDefault()?.Id);
+
+            // Add this line to check the Id of the first item after loading data
+            System.Diagnostics.Debug.WriteLine("First item Id: " + NumberButtonItems.FirstOrDefault()?.Id);
             foreach (var table in NumberButtonItems)
             {
                 Table_Button tableButton = new Table_Button();
@@ -41,11 +43,26 @@ namespace FASTFOOD.User.Pages
         public String SelectedTable
         {
             get { return (String)GetValue(SelectedTableProperty); }
-            set { SetValue(SelectedTableProperty, value); }
+            set
+            {
+                SetValue(SelectedTableProperty, value);
+                NotifyPropertyChanged("Title");
+            }
+        }
+        protected void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
         }
 
         public static DependencyProperty SelectedTableProperty =
            DependencyProperty.Register("SelectedTable", typeof(String), typeof(Tables));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+
         private void Table_Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             // Find the Table_Button in the visual tree hierarchy
@@ -73,6 +90,117 @@ namespace FASTFOOD.User.Pages
             }
 
             return (T)parent;
+        }
+
+
+
+        private void serviceStart_Click(object sender, RoutedEventArgs e)
+        {
+            long serviceID = -1;
+
+            String query;
+            SqlCommand cmd;
+            SqlConnection con = null;
+
+            // Create service
+            try
+            {
+                // Create an instance of SqlConnectionConfiguration
+                SqlConnectionConfiguration connectionConfig = new SqlConnectionConfiguration();
+
+                // Establish connection
+                using (con = connectionConfig.GetSqlConnection())
+                {
+                    con.Open();
+
+                    // Insert into services and retrieve the service ID
+                    query = "INSERT INTO Services (start) VALUES (CURRENT_TIMESTAMP); SELECT SCOPE_IDENTITY();";
+                    cmd = new SqlCommand(query, con);
+
+                    // Use ExecuteScalar to get the result of the SELECT SCOPE_IDENTITY()
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        serviceID = Convert.ToInt64(result);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error creating the service.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return; // Exit the method to avoid further processing
+                    }
+                }
+
+                // Assign to the current table
+                if (serviceID != -1 && SelectedTable != null)
+                {
+                    try
+                    {
+                        // Create an instance of SqlConnectionConfiguration
+                        
+
+                        // Establish connection
+                        using (con = connectionConfig.GetSqlConnection())
+                        {
+                            con.Open();
+
+                            // Update actualServiceID in the tables table
+                            query = "UPDATE tables SET actualServiceID = @serviceID WHERE tables.id = @selectedTableID;";
+                            cmd = new SqlCommand(query, con);
+                            cmd.Parameters.AddWithValue("@serviceID", serviceID);
+
+                            // Check if SelectedTable is not null before calling ToString()
+                            if (SelectedTable != null)
+                            {
+                                cmd.Parameters.AddWithValue("@selectedTableID", SelectedTable.ToString());
+                            }
+                            else
+                            {
+                                MessageBox.Show("SelectedTable is null.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                                return; // Exit the method to avoid further processing
+                            }
+
+                            using (System.Data.IDataReader dr = cmd.ExecuteReader())
+                            {
+                                if (dr.Read())
+                                {
+                                    MessageBox.Show("Error assigning the service to this table.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                                }
+                                else
+                                {
+                                    // TODO OK
+                                    // Reload the view
+                                    LoadData();
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show("There was an error adding the user: \n" + exc.Message.ToString() + ".", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    finally
+                    {
+                        if (con != null)
+                        {
+                            con.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("There was an error adding the user: \n" + exc.Message.ToString() + ".", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+
+
         }
 
 
