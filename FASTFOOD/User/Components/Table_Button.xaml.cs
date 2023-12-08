54535250f45c7966dae8c266d38333c4bab5d8c3
Ali Taghiev <tagiyev_eli@bk.ru>
2023-12-08 04:45:20 +0400

    refactor: Update Table_Button UserControl for external binding
    Resolved an issue with the Table_Button UserControl where external binding was not properly supported. Each property intended for external binding (TableNumber and Service) is now defined as a DependencyProperty. This ensures correct binding when using the Table_Button UserControl.
    
    Changes made:
    - Updated Table_Button.xaml to include DependencyProperty bindings for TableNumber and Service
    - Defined corresponding DependencyProperty properties (TableNumberProperty and ServiceProperty) in Table_Button.xaml.cs

--- FASTFOOD/User/Components/Table_Button.xaml.cs
+++ FASTFOOD/User/Components/Table_Button.xaml.cs
@@ -5,40 +5,27 @@ namespace FASTFOOD.User.Components
 {
     public partial class Table_Button : UserControl
     {
-        public Table_Button()
-        {
-            InitializeComponent();
-            DataContext = this;
-        }
-
-        // Using a DependencyProperty as the backing store for TableNumber. This enables binding.
         public static readonly DependencyProperty TableNumberProperty =
-            DependencyProperty.Register("TableNumber", typeof(string), typeof(Table_Button));
+            DependencyProperty.Register("TableNumber", typeof(int), typeof(Table_Button));
 
-        public string TableNumber
+        public static readonly DependencyProperty ServiceProperty =
+            DependencyProperty.Register("Service", typeof(int), typeof(Table_Button));
+
+        public int TableNumber
         {
-            get { return (string)GetValue(TableNumberProperty); }
+            get { return (int)GetValue(TableNumberProperty); }
             set { SetValue(TableNumberProperty, value); }
         }
 
-        // Loaded event handler for Table_Button
-        private void Table_Button_Loaded(object sender, RoutedEventArgs e)
+        public int Service
         {
-          
-            MessageBox.Show($"Table {TableNumber} loaded.");
-        }
-       
-
-
-        public bool Service
-        {
-            get { return (bool)GetValue(ServiceProperty); }
+            get { return (int)GetValue(ServiceProperty); }
             set { SetValue(ServiceProperty, value); }
         }
 
-        public static DependencyProperty ServiceProperty =
-           DependencyProperty.Register("Service", typeof(bool), typeof(Table_Button));
-    
-
+        public Table_Button()
+        {
+            InitializeComponent();
+        }
     }
 }
--- FASTFOOD/User/Pages/Tables.xaml.cs
+++ FASTFOOD/User/Pages/Tables.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using FASTFOOD.Models;
+using FASTFOOD.User.Components;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace FASTFOOD.User.Pages
@@ -13,12 +17,25 @@ namespace FASTFOOD.User.Pages
             InitializeComponent();
             LoadData();
             DataContext = this; // Set the DataContext to the current instance of the Tables class
+
         }
+        
 
         private void LoadData()
         {
             SqlConnectionConfiguration config = new SqlConnectionConfiguration();
             NumberButtonItems = config.GetTables();
+            
+                // Add this line to check the Id of the first item after loading data
+                System.Diagnostics.Debug.WriteLine("First item Id: " + NumberButtonItems.FirstOrDefault()?.Id);
+            foreach (var table in NumberButtonItems)
+            {
+                Table_Button tableButton = new Table_Button();
+                tableButton.DataContext = table;
+                // Add your Table_Button to the UI
+            }
         }
+       
+
     }
 }
